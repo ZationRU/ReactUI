3449cd1ec3e86004fb16a0175c4e9b40697ee3ec
Лев <levkopo@vk.com>
2023-07-12 19:20:33 +0500

    fix: Switch lagging

--- src/components/Widgets/Switch/Switch.tsx
+++ src/components/Widgets/Switch/Switch.tsx
@@ -1,5 +1,6 @@
 import React, {ChangeEventHandler, ChangeEvent, useRef, useState, useCallback} from "react";
 import "./Switch.css";
+import classNames from "classnames";
 
 export interface SwitchProps {
     onChange?: ChangeEventHandler<HTMLInputElement>
@@ -10,7 +11,6 @@ export function Switch(props: SwitchProps) {
     const switchLayout = useRef<HTMLDivElement>(null)
     const thumb = useRef<HTMLDivElement>(null)
     const checkbox = useRef<HTMLInputElement>(null)
-    const [prevValue, setPrevValue] = useState(props.value || false)
 
     const onClearDown = useCallback(() => {
         if (thumb.current == null) return;
@@ -22,28 +22,36 @@ export function Switch(props: SwitchProps) {
         thumb.current.style.height = thumb.current.style.width = "calc(var(--height) - var(--border-width) * 2)"
     }, [thumb])
 
-    if (prevValue !== props.value) {
-        const checked = props.value || false
-        if (thumb.current == null || switchLayout.current == null) return;
-
-        switchLayout.current.setAttribute("checked", String(checked))
-        thumb.current.setAttribute("checked", String(checked))
-
-        if (checked) {
-            thumb.current.style.height = thumb.current.style.width = "24px"
-            thumb.current.style.transform = "translateX(calc(75%))"
-        } else {
-            thumb.current.style.height = thumb.current.style.width = "16px"
-            thumb.current.style.transform = "translateX(0)"
-        }
-    }
-
-
-    return <div className="Switch" ref={switchLayout} onClick={() => {
-        if (checkbox.current == null) return;
-        checkbox.current.checked = !props.value
-    }} onPointerDown={onDown} onPointerUp={onClearDown} onPointerOut={onClearDown} onMouseLeave={onClearDown}>
-        <div className="Switch-Thumb" ref={thumb}/>
+    return <div
+        className={classNames(
+            "Switch",
+            {
+                "Switch--checked": props.value
+            }
+        )}
+        ref={switchLayout}
+        onClick={() => {
+            if (checkbox.current == null) return;
+            checkbox.current.checked = !props.value
+        }}
+        {...{checked: props.value}}
+        onPointerDown={onDown}
+        onPointerUp={onClearDown}
+        onPointerOut={onClearDown}
+        onMouseLeave={onClearDown}>
+        <div 
+            className="Switch-Thumb"
+            ref={thumb}
+            style={props.value? {
+                height: 24,
+                width: 24,
+                transform: "translateX(calc(75%))"
+            }: {
+                height: 16,
+                width: 16,
+                transform: "translateX(0)"
+            }}
+        />
         <input type="checkbox" checked={props.value} ref={checkbox} onChange={event => {
             if (props.onChange) {
                 props.onChange(event)
