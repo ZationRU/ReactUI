d8a2c0457ad49489c9ca75d16020bb9d8d1648a3
Лев <levkopo@vk.com>
2023-07-12 20:41:43 +0500

    feat: Switch disabled support

--- src/components/Widgets/Switch/Switch.tsx
+++ src/components/Widgets/Switch/Switch.tsx
@@ -1,58 +1,76 @@
-import React, {ChangeEventHandler, ChangeEvent, useRef, useState, useCallback} from "react";
+import React, {ChangeEventHandler, ChangeEvent, useRef, useState, useCallback, ReactNode} from "react";
 import "./Switch.css";
 import classNames from "classnames";
 
 export interface SwitchProps {
     onChange?: ChangeEventHandler<HTMLInputElement>
     value?: boolean
+    icon?: ReactNode
+    disabled?: boolean
 }
 
 export function Switch(props: SwitchProps) {
+    const {
+        value = false,
+        disabled = false,
+        icon,
+    } = props
+
     const switchLayout = useRef<HTMLDivElement>(null)
     const thumb = useRef<HTMLDivElement>(null)
     const checkbox = useRef<HTMLInputElement>(null)
 
     const onClearDown = useCallback(() => {
         if (thumb.current == null) return;
-        thumb.current.style.height = thumb.current.style.width = ''
-    }, [thumb])
+        thumb.current.style.height = thumb.current.style.width = value||icon ? '24px' : '16px';
+        thumb.current.style.marginLeft = thumb.current.style.marginRight = '4px';
+    }, [thumb, value, icon])
 
     const onDown = useCallback(() => {
         if (thumb.current == null) return;
-        thumb.current.style.height = thumb.current.style.width = "calc(var(--height) - var(--border-width) * 2)"
+        thumb.current.style.height = thumb.current.style.width = "28px";
+        thumb.current.style.marginLeft = thumb.current.style.marginRight = '';
     }, [thumb])
 
     return <div
         className={classNames(
             "Switch",
             {
-                "Switch--checked": props.value
+                "Switch--checked": value,
+                "Switch--disabled": disabled,
             }
         )}
         ref={switchLayout}
         onClick={() => {
-            if (checkbox.current == null) return;
-            checkbox.current.checked = !props.value
+            if (checkbox.current == null || disabled) return;
+            checkbox.current.checked = !value
         }}
-        {...{checked: props.value}}
         onPointerDown={onDown}
         onPointerUp={onClearDown}
         onPointerOut={onClearDown}
-        onMouseLeave={onClearDown}>
+        onPointerLeave={onClearDown}
+        onPointerOver={onClearDown}
+        onPointerMove={onClearDown}
+        onPointerCancel={onClearDown}>
         <div 
             className="Switch-Thumb"
             ref={thumb}
-            style={props.value? {
-                height: 24,
-                width: 24,
-                transform: "translateX(calc(75%))"
-            }: {
-                height: 16,
-                width: 16,
-                transform: "translateX(0)"
+            style={{
+                ...(value||icon? {
+                    height: 24,
+                    width: 24,
+                }: {
+                    height: 16,
+                    width: 16,
+                }),
+                marginLeft: 4,
+                marginRight: 4,
+                transform: props.icon ?
+                    (props.value ? "translateX(calc(100% - 8px))" : "translateX(-3px)")
+                :   (props.value ? "translateX(calc(100% - 8px))" : "translateX(0)")
             }}
-        />
-        <input type="checkbox" checked={props.value} ref={checkbox} onChange={event => {
+        >{icon}</div>
+        <input type="checkbox" checked={value} disabled={disabled} ref={checkbox} onChange={event => {
             if (props.onChange) {
                 props.onChange(event)
             }
