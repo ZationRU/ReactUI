a42bf70f0dac35f81c55dd28f18d1d86894e27ca
Лев <levkopo@vk.com>
2023-07-17 00:01:47 +0500

    feat: update Editor in styleguidist

--- src/styleguide/Editor/Editor.tsx
+++ src/styleguide/Editor/Editor.tsx
@@ -6,17 +6,17 @@ import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-jsx';
 import 'prismjs/components/prism-tsx';
 import 'prismjs/components/prism-typescript';
-import 'prismjs/themes/prism.min.css';
+import 'prismjs/themes/prism-twilight.css';
 import './Editor.css';
 import {Editor} from "react-styleguidist/lib/client/rsg-components/Editor/Editor";
 import {Card} from "../../components/Layouts/Card/Card";
 
 export default function EditorWrapper(props: any) {
     return (
-        <Card>
+        <Card bg="var(--znui-dark-background)">
             <Editor {...props} classes={{
                 root: "Editor__inner"
-            }}/>
+            }} style={{ overflow: "auto" }}/>
         </Card>
     );
 }
