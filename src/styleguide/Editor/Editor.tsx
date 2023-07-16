d5b0cd20a0ce597335665c7a172227e20404d6d4
Лев <levkopo@vk.com>
2023-07-17 01:09:09 +0500

    fix: fix usage Editor in styleguidist

--- src/styleguide/Editor/Editor.tsx
+++ src/styleguide/Editor/Editor.tsx
@@ -11,10 +11,17 @@ import './Editor.css';
 import {Editor} from "react-styleguidist/lib/client/rsg-components/Editor/Editor";
 import {Card} from "../../components/Layouts/Card/Card";
 
+let timer: NodeJS.Timeout
+
 export default function EditorWrapper(props: any) {
     return (
-        <Card bg="var(--znui-dark-background)">
-            <Editor {...props} classes={{
+        <Card bg="var(--znui-dark-background)" maxH={400} overflow="auto">
+            <Editor {...props} onChange={(code) => {
+                clearTimeout(timer)
+                timer = setTimeout(() => {
+                    props.onChange(code)
+                }, 3*1000)
+            }} classes={{
                 root: "Editor__inner"
             }} style={{ overflow: "auto" }}/>
         </Card>
