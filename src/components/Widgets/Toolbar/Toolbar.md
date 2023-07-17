06d939c99192c077c70b215742db2becb5f27320
Лев <levkopo@vk.com>
2023-07-17 21:05:39 +0500

    feat: Support menu and navigation icon as IconButton

--- src/components/Widgets/Toolbar/Toolbar.md
+++ src/components/Widgets/Toolbar/Toolbar.md
@@ -1,20 +1,37 @@
 ```js
 import {Layout} from "../../Basic/Layout/Layout"
 import {Button} from "../Button/Button"
-import {ZnUIIconBackArrowFilled} from "@znui/icons"
+import {IconButton} from "../IconButton/IconButton"
+import {ZnUIIconHomeFilled, ZnUIIconUserFilled, ZnUIIconMoreFilled, ZnUIIconBackArrowFilled} from "@znui/icons"
 
 const [navigationIcon, setNavigationIcon] = React.useState(true);
 const [scrolled, setScrolled] = React.useState(true);
+const [enableMenu, updateMenu] = React.useState(true);
 
 const Icon = <ZnUIIconBackArrowFilled/>;
+const Menu = <>
+    <IconButton><ZnUIIconHomeFilled/></IconButton>
+    <IconButton><ZnUIIconUserFilled/></IconButton>
+    <IconButton><ZnUIIconMoreFilled/></IconButton>
+</>;
 
 <Layout display="flex" direction="column" gap={15}>
     <Button onClick={() => setNavigationIcon(!navigationIcon)}>Navigation Icon</Button>
     <Button onClick={() => setScrolled(!scrolled)}>Scroll</Button>
+    <Button onClick={() => updateMenu(!enableMenu)}>Menu</Button>
     
     <Layout display="flex" direction="column" gap={5}>
-        <Toolbar s={scrolled?2:0} navigationIcon={navigationIcon&&Icon}>Title</Toolbar>
-        <Toolbar s={scrolled?2:0} navigationIcon={navigationIcon&&Icon} centered={true}>Centered</Toolbar>
+        <Toolbar 
+            s={scrolled?2:0} 
+            navigationIcon={navigationIcon&&Icon}
+            menu={enableMenu&&Menu}
+        >Title</Toolbar>
+        <Toolbar
+            s={scrolled?2:0} 
+            navigationIcon={navigationIcon&&Icon} 
+            menu={enableMenu&&Menu}
+            centered={true}
+        >Centered</Toolbar>
     </Layout>
 </Layout>
 ```
--- src/components/Widgets/Toolbar/Toolbar.tsx
+++ src/components/Widgets/Toolbar/Toolbar.tsx
@@ -1,15 +1,19 @@
 import classNames from "classnames";
 import './Toolbar.css';
-import React, {ReactNode, MouseEventHandler} from "react";
+import React, {ReactNode, MouseEventHandler, useState, useEffect} from "react";
 import {Title} from "../../Typography/Title/Title";
 import {SurfaceLayout, SurfaceLayoutProps} from "../../Layouts/SurfaceLayout/SurfaceLayout";
 import {StateLayer} from "../../Layouts/StateLayer/StateLayer";
 import {IconWrapper} from "../IconWrapper/IconWrapper";
+import {IconButton} from "../IconButton/IconButton";
+import {Layout} from "../../Basic/Layout/Layout";
+import {FlexLayout, Spacer} from "../../Basic/FlexLayout/FlexLayout";
 
 export interface ToolbarProps extends SurfaceLayoutProps {
     centered?: boolean
     navigationIcon?: ReactNode
     onClickNavigationIcon?: MouseEventHandler<HTMLDivElement>
+    menu?: ReactNode
 }
 
 export function Toolbar(props: ToolbarProps) {
@@ -19,26 +23,42 @@ export function Toolbar(props: ToolbarProps) {
         centered,
         navigationIcon,
         onClickNavigationIcon,
+        menu,
         ...otherProps
     } = props
+
+    const [savedNavigationIcon, saveNavigationIcon] = useState(navigationIcon)
+    useEffect(() => {
+        if(!!navigationIcon) {
+            saveNavigationIcon(navigationIcon)
+        }
+    }, [navigationIcon])
 
     return <SurfaceLayout className={classNames(
         className,
         'Toolbar'
     )} {...otherProps}>
         <div className="inner">
-            <div className={classNames({
+            <IconButton className={classNames({
                 "Toolbar-NavigationIconContainer": true,
                 "Toolbar-NavigationIconContainer--Hidden": !navigationIcon
             })} onClick={onClickNavigationIcon}>
-                <StateLayer/>
-                {navigationIcon&&<IconWrapper>{navigationIcon}</IconWrapper>}
-            </div>
+                {navigationIcon||savedNavigationIcon}
+            </IconButton>
 
             <Title size="large" className={classNames(
                 "Toolbar-Title",
-                centered&&"Toolbar-Title-Centered",
+                {
+                    "Toolbar-Title-Centered": centered,
+                    "Toolbar-Title-Icon--Hidden": !navigationIcon&&!centered
+                },
             )}>{children}</Title>
+
+            <Spacer/>
+
+            {menu&&<FlexLayout className="Toolbar-Menu">
+                {menu}
+            </FlexLayout>}
         </div>
     </SurfaceLayout>
 }
