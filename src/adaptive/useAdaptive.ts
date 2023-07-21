2560d7bc7af83ef6ab0d0f8d2f0fdad85d149ca8
Лев <levkopo@vk.com>
2023-07-21 21:53:50 +0500

    fix: deprecate useAdaptiveProps hook

--- src/adaptive/useAdaptive.ts
+++ src/adaptive/useAdaptive.ts
@@ -2,6 +2,7 @@ import {useContext, useMemo} from "react";
 import {AdaptiveData} from "./AdaptiveData";
 import {AdaptiveContext} from "../components/Providers/AdaptiveProvider/AdaptiveProvider";
 import {resolveAdaptive} from "./AdaptiveResolver";
+import {Adaptive, getAdaptiveValue} from "./Adaptive";
 
 
 
@@ -15,6 +16,14 @@ export const useAdaptive = (): AdaptiveData => {
 }
 
 
+export function useAdaptiveValue<T>(value: Adaptive<T>): T {
+    return getAdaptiveValue(useAdaptive().currentBreakpoint, value)
+}
+
+/**
+ * @deprecated
+ * @param props
+ */
 export function useAdaptiveProps<T>(props: Object): { [key: string]: T } {
     const adaptive = useAdaptive()
     return useMemo(() => resolveAdaptive(adaptive.currentBreakpoint, props), [props, adaptive])
