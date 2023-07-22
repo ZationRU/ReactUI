14dd1567dd577276d80f2980ba47108efb9c21e9
Лев <levkopo@vk.com>
2023-07-22 15:01:10 +0500

    feat: support error and disabled state in TextField

--- src/components/Widgets/TextField/TextField.md
+++ src/components/Widgets/TextField/TextField.md
@@ -8,5 +8,7 @@ import {Layout} from "../../Basic/Layout/Layout";
     <TextField placeholder="Placeholder" label="Label"/>
     <TextField placeholder="Placeholder" label="Label" type="number"/>
     <TextField placeholder="Placeholder" label="Label" type="password"/>
+    <TextField placeholder="Placeholder" label="Label" disabled={true}/>
+    <TextField placeholder="Placeholder" label="Label" error={true} supportingText="Oops.. I'm error"/>
 </Layout>
 ```
--- src/components/Widgets/TextField/TextField.tsx
+++ src/components/Widgets/TextField/TextField.tsx
@@ -4,26 +4,38 @@ import {Layout, LayoutProps} from "../../Basic/Layout/Layout";
 import {HTMLZnUIProps} from "../../../styled/styled.types";
 import {Body} from "../../Typography/Body/Body";
 import classNames from "classnames";
+import {AnimatedVisibility} from "../../Animation/AnimatedVisibility/AnimatedVisibility";
 
 export interface TextFieldProps extends HTMLZnUIProps<"input"> {
     label?: string
+    error?: boolean
+    supportingText?: string
 }
 
 export const TextField = (props: TextFieldProps) => {
     const {
         label,
+        disabled,
+        error = false,
+        supportingText,
         ...inputProps
     } = props
 
-    return <Layout minW={210} pt={6}>
+    return <Layout minW={210} pt={6} oc={disabled?0.32:1} className={classNames({
+        "TextField-Container--error": error
+    })}>
         <Layout h={56} className={classNames(
             "TextField",
             {
-                "TextField--labeled": label
+                "TextField--labeled": label,
             }
         )} overflow="visible">
-            <Layout className="znui-body-large" as="input" {...inputProps}/>
+            <Layout className="znui-body-large" as="input" disabled={disabled} {...inputProps}/>
             {label&&<Body className="label" size="large">{label}</Body>}
         </Layout>
+
+        {supportingText&&<AnimatedVisibility ph={16} pt={4}>
+            <Body size="small">{supportingText}</Body>
+        </AnimatedVisibility>}
     </Layout>
 }
