e566132ffcba2a58df9534a206871e538bf29dd7
Лев <levkopo@vk.com>
2023-07-24 14:35:03 +0500

    feat: improve effect css props documentation

--- src/styled/configs/css/effect.ts
+++ src/styled/configs/css/effect.ts
@@ -18,30 +18,66 @@ Object.assign(effect, {
 
 export interface EffectProps {
     /**
-     * The `box-shadow` property
+     * The CSS `box-shadow` property
+     *
+     * @default undefined
+     * @link https://developer.mozilla.org/en-US/docs/Web/CSS/box-shadow
      */
     boxShadow?: Adaptive<CSS.Property.BoxShadow | number>
+
+    /**
+     * The CSS `box-shadow` property
+     *
+     * @default undefined
+     * @link https://developer.mozilla.org/en-US/docs/Web/CSS/box-shadow
+     */
     shadow?: Adaptive<CSS.Property.BoxShadow | number>
 
     /**
-     * The `mix-blend-mode` property
+     * The CSS `mix-blend-mode` property
+     *
+     * @default undefined
+     * @link https://developer.mozilla.org/en-US/docs/Web/CSS/mix-blend-mode
      */
     mixBlendMode?: Adaptive<CSS.Property.MixBlendMode>
+
     /**
-     * The `blend-mode` property
+     * The CSS `blend-mode` property
+     *
+     * @default undefined
+     * @link https://developer.mozilla.org/en-US/docs/Web/CSS/blend-mode
      */
     blendMode?: Adaptive<CSS.Property.MixBlendMode>
 
     /**
      * The CSS `background-blend-mode` property
+     *
+     * @default undefined
+     * @link https://developer.mozilla.org/en-US/docs/Web/CSS/background-blend-mode
      */
     backgroundBlendMode?: Adaptive<CSS.Property.BackgroundBlendMode>
+
+    /**
+     * The CSS `background-blend-mode` property
+     *
+     * @default undefined
+     * @link https://developer.mozilla.org/en-US/docs/Web/CSS/background-blend-mode
+     */
     bgBlendMode?: Adaptive<CSS.Property.BackgroundBlendMode>
 
     /**
      * The CSS `opacity` property
-     * @default 1
+     *
+     * @link https://developer.mozilla.org/en-US/docs/Web/CSS/opacity
+     * @default undefined
      */
     opacity?: Adaptive<CSS.Property.Opacity | number>
+
+    /**
+     * The CSS `opacity` property
+     *
+     * @link https://developer.mozilla.org/en-US/docs/Web/CSS/opacity
+     * @default undefined
+     */
     oc?: Adaptive<CSS.Property.Opacity | number>
 }
