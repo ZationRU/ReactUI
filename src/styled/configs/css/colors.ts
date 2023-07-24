53eeebaad4f18584a039a24d0bb66d89ad939697
Лев <levkopo@vk.com>
2023-07-24 14:33:27 +0500

    feat: improve colors css props documentation

--- src/styled/configs/css/colors.ts
+++ src/styled/configs/css/colors.ts
@@ -6,31 +6,51 @@ export const colors = {
     color: asCSSProp("color"),
     fill: asCSSProp("fill"),
     stroke: asCSSProp("stroke"),
-    opacity: asCSSProp("opacity"),
 }
 
 Object.assign(colors, {
     textColor: colors.color,
     c: colors.color,
-    oc: colors.opacity
 })
 
 export interface ColorsProps {
     /**
-     * Text color
-     * @default currentColor
+     * The CSS `color` property
+     *
+     * @default undefined
+     * @link https://developer.mozilla.org/en-US/docs/Web/CSS/color
      */
     textColor?: Adaptive<CSS.Property.Color>
+
+    /**
+     * The CSS `color` property
+     *
+     * @default undefined
+     * @link https://developer.mozilla.org/en-US/docs/Web/CSS/color
+     */
     color?: Adaptive<CSS.Property.Color|string>
+
+    /**
+     * The CSS `color` property
+     *
+     * @default undefined
+     * @link https://developer.mozilla.org/en-US/docs/Web/CSS/color
+     */
     c?: Adaptive<CSS.Property.Color|string>
 
     /**
      * The CSS `fill` property for icon svgs and paths
+     *
+     * @default undefined
+     * @link https://developer.mozilla.org/en-US/docs/Web/CSS/fill
      */
     fill?: Adaptive<CSS.Property.Color>
 
     /**
      * The CSS `stroke` property for icon svgs and paths
+     *
+     * @default undefined
+     * @link https://developer.mozilla.org/en-US/docs/Web/CSS/stroke
      */
     stroke?: Adaptive<CSS.Property.Color>
 }
