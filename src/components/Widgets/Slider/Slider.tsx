f4d83d0798083ccf483437294b5c7e34ba4bc826
Лев <levkopo@vk.com>
2023-08-04 00:55:18 +0500

    fix(Slider): usability fixes

--- src/components/Widgets/Slider/Slider.tsx
+++ src/components/Widgets/Slider/Slider.tsx
@@ -1,6 +1,7 @@
 import React, {PointerEvent, useCallback, useRef, useState} from "react";
 import {Layout} from "../../Basic/Layout/Layout";
 import {HTMLZnUIProps} from "../../../styled/styled.types";
+import {FlexLayout} from "../../Basic/FlexLayout/FlexLayout";
 
 export interface SliderProps {
     /**
@@ -29,13 +30,14 @@ export interface SliderProps {
 
     /**
      * Step of value change
-     * @default 0
+     * @default 1
      */
     step?: number
 
     /**
      * Changed value listener
      * @param value
+     * @default undefined
      */
     onChange?: (value: number) => number;
 }
@@ -59,16 +61,18 @@ export function Slider(props: SliderProps) {
         min = 0,
         value,
         defaultValue = 0,
-        step = 0,
+        step = 1,
         onChange
     } = props
 
-    const stepCount = step===0 ? 0 : (max-min) / step
-    const steps = Array.from({ length: stepCount===0? 0: stepCount+1 }, (_, i) => Math.round(i*(100/step)))
-    const trackWidth = (value || defaultValue) / (max-min) * 100;
+    const _currentValue = (value || defaultValue)
+    const stepCount = (max-min) / step
+
+    const currentValue = _currentValue - (step===1? 0: (_currentValue % stepCount))
+    const trackWidth = currentValue / (max-min) * 100;
 
     let pressed = false;
-    let newValue = value || defaultValue;
+    let newValue = currentValue;
     const performUp = () => {
         console.log("up")
         pressed = false
@@ -79,65 +83,59 @@ export function Slider(props: SliderProps) {
         pressed = true
     }
 
-    return <Layout
-        h={20}
-        ph={3}
-        pv={3}
-        pos="relative"
-        overflow="visible"
-        userSelect="none"
-        cursor="pointer"
-        ref={sliderLayoutRef}
-        onPointerMove={(e: PointerEvent<HTMLDivElement>) => {
-            if(!pressed||
-                sliderLayoutRef.current==null||
-                activeTrackRef.current==null ||
-                handleRef.current==null) {
-                return;
-            }
-
-            const rect = sliderLayoutRef.current.getBoundingClientRect();
-            let x = (e.clientX-rect.left);
-            if(x<0) x = 0;
-
-            let percentage = x/(rect.width+20);
-            if(percentage>1) percentage = 1;
-            if(steps.length!==0) {
-                console.log(steps)
-                const step = steps.find(it => it>=percentage*100)
-                percentage = step === undefined ?
-                    Math.ceil(percentage/100) === 0 ? 0
-                        : Math.round(percentage/100)
-                    : step / 100
-
-            }
-
-            handleRef.current.style.left = activeTrackRef.current.style.maxWidth = (percentage*100) + "%"
-            newValue = min + (percentage * (max-min));
-        }}
-
-        style={{
-            touchAction: "none"
-        }}
-        onPointerUp={performUp}
-        onPointerCancel={performUp}
-        onPointerLeave={performUp}
-        onPointerDown={performDown}>
-
+    return <Layout>
         <Layout
-            pos="absolute"
-            w="100%"
-            h={4}
-            left={10}
-            top="calc(50% - 2px)"
-        >
+            h={20}
+            m={3}
+            pos="relative"
+            overflow="visible"
+            userSelect="none"
+            cursor="pointer"
+            ref={sliderLayoutRef}
+            onPointerMoveCapture={(e: PointerEvent<HTMLDivElement>) => {
+                if(!pressed||
+                    sliderLayoutRef.current==null||
+                    activeTrackRef.current==null ||
+                    handleRef.current==null) {
+                    return;
+                }
+
+                const rect = sliderLayoutRef.current.getBoundingClientRect();
+                let x = (e.clientX-rect.left);
+                if(x<0) x = 0;
+
+                let percentage = x/(rect.width);
+                if(percentage>1) percentage = 1;
+
+                newValue = min + (percentage * (max-min));
+
+                if(max-newValue<step/2) {
+                    newValue = max
+                }
+
+                let mod = (step===1? 0: (newValue % stepCount))
+                newValue = Math.round(newValue - mod)
+
+                const finalPercentage = (newValue-min/(max-min)*100)
+                handleRef.current.style.left = "calc(" + finalPercentage + "% - 10px)";
+                activeTrackRef.current.style.maxWidth = finalPercentage + "%";
+            }}
+
+            style={{
+                touchAction: "none"
+            }}
+            onPointerUp={performUp}
+            onPointerCancel={performUp}
+            onPointerLeave={performUp}
+            onPointerDown={performDown}>
 
             <Layout
                 as="input"
                 type="range"
                 pos="absolute"
                 h={20}
-                w="100%"
+                left={0}
+                right={0}
                 oc={0}
                 min={min}
                 max={max}
@@ -150,64 +148,76 @@ export function Slider(props: SliderProps) {
             />
 
             <Layout
-                as="span"
                 pos="absolute"
+                left={10}
+                right={10}
                 h={4}
-                borderRadius={4}
-                w="100%"
-                bg="var(--znui-surface-variant)"
+                overflow="visible"
+                top="calc(50% - 2px)"
             >
-                {
-                    steps.map((it, i) =>
-                        <Layout
-                            key={"step-"+i}
-                            left={"calc("+it+"% + 1px)"}
-                            pos="absolute"
-                            top={1}
-                            layoutSize={2}
-                            bg="var(--znui-on-surface-variant)"
-                        />
-                    )
-                }
+
+                <Layout
+                    as="span"
+                    pos="absolute"
+                    h={4}
+                    borderRadius={4}
+                    left={0}
+                    right={0}
+                    bg="var(--znui-surface-variant)"
+                >
+                    <FlexLayout
+                        w="calc(100% - 3px)"
+                        justify="space-between"
+                        ml={1}
+                        mt={1}
+                    >
+                        {
+                            Array.from({ length: step<=1 ? 0: stepCount }).map((it, i) =>
+                                <Layout
+                                    key={"step-"+i}
+                                    layoutSize={2}
+                                    shapeScale="full"
+                                    bg="var(--znui-on-surface-variant)"
+                                />
+                            )
+                        }
+                    </FlexLayout>
+                </Layout>
+
+                <Layout
+                    as="span"
+                    pos="absolute"
+                    h={4}
+                    borderRadius={4}
+                    left={0}
+                    right={0}
+                    ref={activeTrackRef}
+                    maxW={trackWidth+"%"}
+                    bg="var(--znui-primary)"
+                >
+                    {
+                        Array.from({ length: step<=1 ? 0: stepCount }).map((it, i) =>
+                            <Layout
+                                key={"step-"+i}
+                                layoutSize={2}
+                                shapeScale="full"
+                                bg="var(--znui-on-surface-variant)"
+                            />
+                        )
+                    }
+                </Layout>
+
+                <Layout
+                    pos="absolute"
+                    shapeScale="full"
+                    overflow="visible"
+                    className="elevation-1"
+                    left={"calc("+trackWidth+"% - 10px)"}
+                    bg="var(--znui-primary)"
+                    ref={handleRef}
+                    top={-10}
+                    layoutSize={20}/>
             </Layout>
-
-            <Layout
-                as="span"
-                pos="absolute"
-                h={4}
-                borderRadius={4}
-                w="100%"
-                ref={activeTrackRef}
-                maxW={trackWidth+"%"}
-                bg="var(--znui-primary)"
-            >
-                {
-                    steps.map((it, i) =>
-                        <Layout
-                            key={"step-"+i}
-                            left={"calc("+it+"% + 1px)"}
-                            pos="absolute"
-                            top={1}
-                            layoutSize={2}
-                            bg="var(--znui-on-surface-variant)"
-                        />
-                    )
-                }
-            </Layout>
-
-            {
-
-            }
         </Layout>
-
-        <Layout
-            pos="absolute"
-            shapeScale="full"
-            overflow="visible"
-            className="elevation-1"
-            left={trackWidth+"%"}
-            bg="var(--znui-primary)"
-            ref={handleRef}
-            layoutSize={20}/>
     </Layout>
 }
