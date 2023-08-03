dc13218210faa3e44330de3984e859955be20bd6
Лев <levkopo@vk.com>
2023-08-04 01:00:11 +0500

    fix(insets): export insets props types

--- src/styled/configs/znui/index.ts
+++ src/styled/configs/znui/index.ts
@@ -2,6 +2,7 @@ import {shapes, ShapesProps} from "./shapes";
 import {insets, InsetsProps} from "./insets";
 
 export * from "./shapes"
+export * from "./insets"
 
 export const znuiPropsConfig = {
     ...shapes,
