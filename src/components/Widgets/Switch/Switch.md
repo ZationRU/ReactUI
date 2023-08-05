a9f54338e6196b96d49615b5b09aa6b7b3e1fcfc
Лев <levkopo@vk.com>
2023-08-06 00:38:52 +0500

    fix(Switch): change value to checked

--- src/components/Widgets/Switch/Switch.md
+++ src/components/Widgets/Switch/Switch.md
@@ -9,7 +9,7 @@ const Icon = () => value ? <ZnUIIconCloseFilled/> : <ZnUIIconVerifiedFilled/>;
 
 <Layout display="flex" direction="column" gap={5}>
     <Switch value={disabled} onChange={(e) => setDisabled(e.currentTarget.checked)}/>
-    <Switch disabled={disabled} value={value} onChange={(e) => setValue(e.currentTarget.checked)}/>
-    <Switch disabled={disabled} icon={<Icon/>} value={!value} onChange={(e) => setValue(!e.currentTarget.checked)}/>
+    <Switch disabled={disabled} checked={value} onChange={(e) => setValue(e.currentTarget.checked)}/>
+    <Switch disabled={disabled} icon={<Icon/>} checked={!value} onChange={(e) => setValue(!e.currentTarget.checked)}/>
 </Layout>
 ```
--- src/components/Widgets/Switch/Switch.tsx
+++ src/components/Widgets/Switch/Switch.tsx
@@ -1,11 +1,11 @@
-import React, {ChangeEventHandler, ChangeEvent, useRef, useState, useCallback, ReactNode} from "react";
+import React, {ChangeEventHandler, ChangeEvent, useRef, useState, useCallback, ReactNode, ForwardedRef} from "react";
 import "./Switch.css";
 import classNames from "classnames";
 import {IconWrapper} from "../IconWrapper/IconWrapper";
 
 export interface SwitchProps {
     onChange?: ChangeEventHandler<HTMLInputElement>
-    value?: boolean
+    checked?: boolean
     icon?: ReactNode
     disabled?: boolean
 }
@@ -16,9 +16,9 @@ export interface SwitchProps {
  * @param props
  * @constructor
  */
-export function Switch(props: SwitchProps) {
+export const Switch = React.forwardRef((props: SwitchProps, ref: ForwardedRef<HTMLInputElement>) => {
     const {
-        value = false,
+        checked = false,
         disabled = false,
         icon,
         onChange
@@ -30,30 +30,30 @@ export function Switch(props: SwitchProps) {
 
     const onClearDown = useCallback(() => {
         if (thumb.current == null) return;
-        thumb.current.style.height = thumb.current.style.width = value||icon ? '24px' : '16px';
-        thumb.current.style.marginLeft = !value&&!icon ? '8px' : '4px';
-        thumb.current.style.transform = value ? "translateX(calc(100% - 4px))" : "translateX(0)";
-    }, [thumb, value, icon])
+        thumb.current.style.height = thumb.current.style.width = checked||icon ? '24px' : '16px';
+        thumb.current.style.marginLeft = !checked&&!icon ? '8px' : '4px';
+        thumb.current.style.transform = checked ? "translateX(calc(100% - 4px))" : "translateX(0)";
+    }, [thumb, checked, icon])
 
     const onDown = useCallback(() => {
         if (thumb.current == null) return;
         thumb.current.style.height = thumb.current.style.width = "28px";
-        thumb.current.style.marginLeft = value ? '': '3px';
-        thumb.current.style.transform = value ? "translateX(calc(100% - 6px))" : "translateX(0)";
-    }, [thumb, value])
+        thumb.current.style.marginLeft = checked ? '': '3px';
+        thumb.current.style.transform = checked ? "translateX(calc(100% - 6px))" : "translateX(0)";
+    }, [thumb, checked])
 
     return <div
         className={classNames(
             "Switch",
             {
-                "Switch--checked": value,
+                "Switch--checked": checked,
                 "Switch--disabled": disabled,
             }
         )}
         ref={switchLayout}
         onClick={() => {
             if (checkbox.current == null || disabled) return;
-            checkbox.current.checked = !value
+            checkbox.current.checked = !checked
         }}
         onPointerDown={onDown}
         onPointerUp={onClearDown}
@@ -69,22 +69,22 @@ export function Switch(props: SwitchProps) {
             className="Switch-Thumb"
             ref={thumb}
             style={{
-                ...(value||icon? {
+                ...(checked||icon? {
                     height: 24,
                     width: 24,
                 }: {
                     height: 16,
                     width: 16,
                 }),
-                marginLeft: !value&&!icon ? 8 : 4,
+                marginLeft: !checked&&!icon ? 8 : 4,
                 marginRight: 4,
-                transform: value ? "translateX(calc(100% - 4px))" : "translateX(0)"
+                transform: checked ? "translateX(calc(100% - 4px))" : "translateX(0)"
             }}
         ><IconWrapper>{icon}</IconWrapper></div>
-        <input type="checkbox" checked={value} disabled={disabled} ref={checkbox} onChange={event => {
+        <input type="checkbox" checked={checked} disabled={disabled} ref={checkbox} onChange={event => {
             if (onChange) {
                 onChange(event)
             }
         }}/>
     </div>
-}
+})
