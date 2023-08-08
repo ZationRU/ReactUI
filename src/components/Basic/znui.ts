30d21a1082d65c5fdff235c739a4727be3b7c2f4
Лев <levkopo@vk.com>
2023-08-08 16:17:13 +0500

    feat(znui): new system of ZnUIComponents factory

--- src/components/Basic/znui.ts
+++ src/components/Basic/znui.ts
@@ -1,10 +1,31 @@
 import React from "react";
-import {HTMLZnUIProps, ZnUIComponent} from "../../styled/styled.types";
+import {
+    HTMLZnUIComponents,
+    HTMLZnUIProps,
+    JSXElements,
+    ZnUIComponent,
+    ZnUIStyleObject
+} from "../../styled/styled.types";
 import {styled} from "../../styled/styled";
 
-export function znui<T extends React.ElementType>(
-    element: T,
-    baseProps: HTMLZnUIProps<T>|null = null
-): ZnUIComponent<T, HTMLZnUIProps<T>> {
-    return styled<T, HTMLZnUIProps<T>>(element, baseProps)
+const cache = new Map<JSXElements, ZnUIComponent<JSXElements>>()
+
+type ZnUIFactory = {
+    <T extends React.ElementType, P extends object = {}>(
+        component: T,
+        options?: ZnUIStyleObject,
+    ): ZnUIComponent<T, P>
 }
+
+export const znui = new Proxy(styled, {
+    apply(target, thisArg, argArray: [JSXElements, ZnUIStyleObject]) {
+        return styled(...argArray)
+    },
+
+    get(_, element: JSXElements) {
+        if (!cache.has(element)) {
+            cache.set(element, styled(element))
+        }
+        return cache.get(element)
+    },
+}) as ZnUIFactory & HTMLZnUIComponents
--- src/styled/styled.types.ts
+++ src/styled/styled.types.ts
@@ -1,9 +1,33 @@
 import {
     CSSProps, ZnUICSSProps
 } from "./configs";
+import * as CSS from "csstype";
 import React from "react";
 
 export type StyleProps = CSSProps & ZnUICSSProps;
+
+export interface SystemCSSProperties
+    extends CSS.Properties,
+        Omit<StyleProps, keyof CSS.Properties> {}
+
+
+type PropertyValue<K extends keyof SystemCSSProperties> = SystemCSSProperties[K]
+
+export type CSSWithMultiValues = {
+    [K in keyof SystemCSSProperties]?: K extends keyof StyleProps
+        ? StyleProps[K] | PropertyValue<K>
+        : PropertyValue<K>
+}
+
+type CSSDefinition<D> = D | string | RecursiveCSSSelector<D | string>
+
+export interface RecursiveCSSSelector<D> {
+    [selector: string]: CSSDefinition<D> & D
+}
+
+export type RecursiveCSSObject<D> = D & (D | RecursiveCSSSelector<D>)
+
+export type ZnUIStyleObject = RecursiveCSSObject<CSSWithMultiValues>
 
 export interface ZnUIProps extends StyleProps {
 
@@ -67,3 +91,10 @@ export type Component<Component extends React.ElementType, Props extends object 
 
 export interface ZnUIComponent<T extends React.ElementType, P extends object = {}>
     extends Component<T, Assign<ZnUIProps, P>> {}
+
+export type JSXElements = keyof JSX.IntrinsicElements
+
+export type HTMLZnUIComponents = {
+    [Tag in JSXElements]: ZnUIComponent<Tag, {}>
+}
+
