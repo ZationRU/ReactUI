dd7d7f8b3bc1096d2f832031d48e3b8ae945283e
Лев <levkopo@vk.com>
2023-08-08 16:58:25 +0500

    feat(Layout): simplify Layout creating

--- src/components/Basic/Layout/Layout.tsx
+++ src/components/Basic/Layout/Layout.tsx
@@ -1,11 +1,9 @@
 import {znui} from "../znui";
 import {HTMLZnUIProps} from "../../../styled/styled.types";
-import React, {RefAttributes} from "react";
 
 export type LayoutProps = HTMLZnUIProps<"div">
 
 /**
  * Basic component for all znui components
  */
-export const Layout = znui("div")
-export const InputLayout = znui("input")
+export const Layout = znui.div
