c0b347c3674c51f8fbf4c39245ab6ad560769225
Лев <levkopo@vk.com>
2023-08-09 17:46:29 +0500

    feat(Button): new modes: tonal and elevated

--- src/components/Widgets/Button/Button.tsx
+++ src/components/Widgets/Button/Button.tsx
@@ -7,7 +7,7 @@ import {HTMLZnUIProps} from "../../../styled/styled.types";
 import {znui} from "../../Basic/znui";
 
 export interface ButtonProps extends HTMLZnUIProps<'button'>{
-    mode?: 'filled'|'text'|'outline',
+    mode?: 'filled'|'text'|'outline'|'tonal'|'elevated',
     icon?: ReactNode
 }
 
@@ -25,9 +25,15 @@ export function Button(props: ButtonProps) {
         ...otherProps
     } = props
 
-    return <znui.button as="button" className={
-        classNames(className, 'Button', 'Button--'+mode)
-    } {...otherProps}>
+    return <znui.button
+        as="button"
+        className={
+            classNames(className, 'Button', 'Button--'+mode, {
+                'elevation-1': mode==='elevated'
+            })
+        }
+        {...otherProps}
+    >
         <StateLayer/>
 
         <div className="inner">
