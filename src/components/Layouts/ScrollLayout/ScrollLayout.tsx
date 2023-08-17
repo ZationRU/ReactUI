35f200ba520c86ae93f314aa961e4a6494b0b11f
levkopo <levkopoag@gmail.com>
2023-08-17 15:55:45 +0500

    feat(ScrollLayout): support orientation prop in ScrollLayout

--- src/components/Layouts/ScrollLayout/ScrollLayout.tsx
+++ src/components/Layouts/ScrollLayout/ScrollLayout.tsx
@@ -1,8 +1,15 @@
 import {znui} from "../../Basic/znui";
+import {ZnUIComponent} from "../../../styled/styled.types";
 
-export const ScrollLayout = znui('div', {
+export const ScrollLayout: ZnUIComponent<'div', {
+    orientation?: 'vertical'|'horizontal'
+}> = znui('div', {
     baseStyle: {
-        overflow: 'scroll',
+        overflow: 'auto',
         w: '100%'
-    }
+    },
+    styles: props => ({
+        overflowY: props.orientation !== 'horizontal' ? 'scroll': 'clip',
+        overflowX: props.orientation === 'horizontal' ? 'scroll': 'clip'
+    })
 })
