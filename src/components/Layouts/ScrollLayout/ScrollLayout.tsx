022fbc6b626fcaeb157008e7d4554ff8a25c20e3
levkopo <levkopoag@gmail.com>
2023-08-17 16:23:11 +0500

    fix(ScrollLayout): props fix

--- src/components/Layouts/ScrollLayout/ScrollLayout.tsx
+++ src/components/Layouts/ScrollLayout/ScrollLayout.tsx
@@ -4,12 +4,9 @@ import {ZnUIComponent} from "../../../styled/styled.types";
 export const ScrollLayout: ZnUIComponent<'div', {
     orientation?: 'vertical'|'horizontal'
 }> = znui('div', {
-    baseStyle: {
-        overflow: 'auto',
-        w: '100%'
-    },
     styles: props => ({
         overflowY: props.orientation !== 'horizontal' ? 'scroll': 'clip',
+        w: props.orientation !== 'horizontal' ? '100%': undefined,
         overflowX: props.orientation === 'horizontal' ? 'scroll': 'clip'
     })
 })
