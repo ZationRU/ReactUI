3d684674eb4a7a128a5e0086f10a7fbf6aab39f1
levkopo <levkopoag@gmail.com>
2023-08-17 18:20:46 +0500

    fix(Modal): actions in non-fullscreen mode

--- src/components/Widgets/Modal/Modal.tsx
+++ src/components/Widgets/Modal/Modal.tsx
@@ -3,8 +3,8 @@ import {Layout} from "../../Basic/Layout/Layout";
 import {useAdaptiveValue} from "../../../adaptive/useAdaptive";
 import {Toolbar} from "../Toolbar/Toolbar";
 import {IconButton} from "../IconButton/IconButton";
-import {Spacer} from "../../Basic/FlexLayout/FlexLayout";
-import {HStack} from "../../Basic/Stack/Stack";
+import {FlexLayout, Spacer} from "../../Basic/FlexLayout/FlexLayout";
+import {HStack, VStack} from "../../Basic/Stack/Stack";
 import {CoordinatorLayout} from "../../Layouts/CoordinatorLayout/CoordinatorLayout";
 import {AppBarLayout} from "../../Layouts/AppBarLayout/AppBarLayout";
 import {ScrollLayout} from "../../Layouts/ScrollLayout/ScrollLayout";
@@ -37,29 +37,31 @@ export function Modal(props: ModalWrapperProps) {
         md: false
     });
 
-    return <CoordinatorLayout
+    return <VStack
         maxH={isFullscreen? '100vh': '60vh'}
         insets={isFullscreen ? "safe-area": undefined}
     >
-        <AppBarLayout>
-            <Toolbar
-                navigationIcon={isFullscreen ? navigationIcon: undefined}
-                onClickNavigationIcon={onClickNavigationIcon}
-                menu={isFullscreen? action : <IconButton onClick={onClickNavigationIcon}>
-                    {navigationIcon}
-                </IconButton>}
-            >{title}</Toolbar>
-        </AppBarLayout>
+        <CoordinatorLayout maxH="100%">
+            <AppBarLayout>
+                <Toolbar
+                    navigationIcon={isFullscreen ? navigationIcon: undefined}
+                    onClickNavigationIcon={onClickNavigationIcon}
+                    menu={isFullscreen? action : <IconButton onClick={onClickNavigationIcon}>
+                        {navigationIcon}
+                    </IconButton>}
+                >{title}</Toolbar>
+            </AppBarLayout>
 
-        <ScrollLayout orientation="vertical" behavior={AppBarLayout.ScrollBehavior}>
-            <Layout ph={24} pb={24}>
-                {children}
-            </Layout>
-        </ScrollLayout>
+            <ScrollLayout orientation="vertical" behavior={AppBarLayout.ScrollBehavior}>
+                <Layout ph={24} pb={24}>
+                    {children}
+                </Layout>
+            </ScrollLayout>
+        </CoordinatorLayout>
 
         {!isFullscreen&&<HStack spacing={16} pv={24} pr={24} pl={16}>
             <Spacer/>
             {action}
         </HStack>}
-    </CoordinatorLayout>
+    </VStack>
 }
