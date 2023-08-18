46df2aa2a5cdb07acb058b53f74fc9b197296a66
levkopo <levkopoag@gmail.com>
2023-08-18 22:25:42 +0500

    fix(Playground): change height in Editor

--- src/styleguide/PlaygroundRenderer/PlaygroundRenderer.tsx
+++ src/styleguide/PlaygroundRenderer/PlaygroundRenderer.tsx
@@ -11,13 +11,13 @@ interface PlaygroundRendererProps {
 
 const PlaygroundRenderer = ({ name, preview, tabBody }: PlaygroundRendererProps) => {
     return (
-        <Layout bg={ThemeTokens.surfaceContainerLowest} shapeScale="lg">
+        <Layout bg={ThemeTokens.surfaceContainerLow} shapeScale="lg">
             <FlexLayout w="100%" direction={[
                 'column-reverse',
                 null,
                 'row'
             ]}>
-                <Layout flex={1}>{tabBody}</Layout>
+                <Layout maxH={600} minH={600} flex={1}>{tabBody}</Layout>
                 <Layout bg={ThemeTokens.surfaceBright} p={15} data-preview={name} flex={1} overflow="visible">
                     {preview}
                 </Layout>
