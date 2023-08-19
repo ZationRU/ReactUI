56485cd4203966e065f349a186ce1df99cb2699a
levkopo <levkopoag@gmail.com>
2023-08-19 20:47:05 +0500

    fix(PlaygroundRenderer): code scroll

--- src/styleguide/PlaygroundRenderer/PlaygroundRenderer.tsx
+++ src/styleguide/PlaygroundRenderer/PlaygroundRenderer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Layout} from "../../components/Basic/Layout/Layout";
 import {FlexLayout} from "../../components/Basic/FlexLayout/FlexLayout";
 import {ThemeTokens} from "../../theme";
+import {ScrollLayout} from "../../components/Layouts/ScrollLayout/ScrollLayout";
 
 interface PlaygroundRendererProps {
     name?: string;
@@ -15,9 +16,10 @@ const PlaygroundRenderer = ({ name, preview, tabBody }: PlaygroundRendererProps)
             <FlexLayout w="100%" direction={[
                 'column-reverse',
                 null,
+                null,
                 'row'
             ]}>
-                <Layout maxH={600} minH={600} flex={1}>{tabBody}</Layout>
+                <ScrollLayout maxH={600} minH={600} flex={1}>{tabBody}</ScrollLayout>
                 <Layout bg={ThemeTokens.surfaceBright} p={15} data-preview={name} flex={1} overflow="visible">
                     {preview}
                 </Layout>
