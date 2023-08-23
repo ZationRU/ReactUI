23947b00bf54696832e627f004f1f204b12f2d77
levkopo <levkopoag@gmail.com>
2023-08-23 10:57:03 +0500

    feat(SwipeRefreshLayout): new component

---
+++ src/components/Layouts/SwipeRefreshLayout/SwipeRefreshLayout.md
@@ -0,0 +1,68 @@
+```tsx
+import {
+    CoordinatorLayout, Toolbar, VStack, HStack, Avatar, Body,
+    IconButton, Button, Card, Title, Layout, ThemeTokens, AppBarLayout,
+    ScrollLayout
+} from "@znui/react";
+
+import {ZnUIIconMoreFilled} from "@znui/icons";
+import {useState} from "react";
+
+
+const onRefresh = (e) => {
+    setTimeout(() => {
+        e.complete()
+    }, 5000)
+};
+
+<CoordinatorLayout bg={ThemeTokens.surface} h={600} maxW={400} border="black solid 1px">
+    <AppBarLayout>
+        <Toolbar>Title</Toolbar>
+    </AppBarLayout>
+
+    <SwipeRefreshLayout
+        onRefresh={onRefresh}
+        behavior={AppBarLayout.ScrollBehavior}
+        height="100%">
+        <VStack spacing={10}>
+            {
+                Array.from({length: 100}, () => 0).map((_, i) =>
+                    <Card key={i}>
+                        <HStack pl={16} pr={4} pv={12} align="center">
+                            <HStack flex={1} spacing={16} align="center">
+                                <Avatar size={40} text="A"/>
+
+                                <VStack spacing={4}>
+                                    <Title size="medium">Heading</Title>
+                                    <Body size="medium">Subhead</Body>
+                                </VStack>
+                            </HStack>
+
+                            <IconButton>
+                                <ZnUIIconMoreFilled/>
+                            </IconButton>
+                        </HStack>
+                        <Layout h={188} bg={ThemeTokens.surfaceContainerLowest}/>
+
+                        <VStack spacing={32} p={16}>
+                            <VStack>
+                                <Title size="large">Title</Title>
+                                <Body size="medium" c={ThemeTokens.onSurfaceVariant}>Subhead</Body>
+                            </VStack>
+
+                            <Body size="medium" c={ThemeTokens.onSurfaceVariant}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                            </Body>
+
+                            <HStack justify="end" spacing={8}>
+                                <Button mode="outline">Enabled</Button>
+                                <Button>Enabled</Button>
+                            </HStack>
+                        </VStack>
+                    </Card>
+                )
+            }
+        </VStack>
+    </SwipeRefreshLayout>
+</CoordinatorLayout>
+```
---
+++ src/components/Layouts/SwipeRefreshLayout/SwipeRefreshLayout.tsx
@@ -0,0 +1,100 @@
+import React, {ForwardedRef, useCallback, useState, PointerEvent} from "react";
+import {ScrollLayout} from "../ScrollLayout/ScrollLayout";
+import {LayoutProps} from "../../Basic/Layout/Layout";
+import {ThemeTokens} from "../../../theme";
+import {CircularProgressIndicator} from "../../Widgets/CircularProgressIndicator/CircularProgressIndicator";
+import {Center} from "../../Basic/Center/Center";
+
+export interface SwipeRefreshLayoutProps extends LayoutProps{
+    onRefresh: (event: RefreshEvent) => void
+}
+
+export interface RefreshEvent {
+    complete: () => void
+}
+
+export const SwipeRefreshLayout = React.forwardRef((
+    props: SwipeRefreshLayoutProps,
+    ref: ForwardedRef<HTMLDivElement>
+) => {
+    const [isRefreshing, setIsRefreshing] = useState(false)
+    const [refreshPercentage, setRefreshPercentage] = useState(0)
+    const [transition, setTransition] = useState<string|undefined>()
+    const {
+        onRefresh,
+        children,
+        ...otherProps
+    } = props
+
+    const refresh = useCallback((e: PointerEvent<HTMLDivElement>) => {
+        if(!isRefreshing) {
+            e.currentTarget.removeAttribute('data-y-down')
+            if(refreshPercentage>=1){
+                onRefresh?.call(undefined, {
+                    complete: () => {
+                        setTransition('top 300ms var(--emphasized-motion)')
+                        setRefreshPercentage(0)
+                        setIsRefreshing(false)
+                    }
+                })
+                setIsRefreshing(true)
+            }else{
+                setTransition('top 300ms var(--emphasized-motion)')
+                setRefreshPercentage(0)
+            }
+        }
+    }, [isRefreshing, onRefresh, refreshPercentage])
+
+    const currentPercentage = isRefreshing ? 1: refreshPercentage
+
+    return <ScrollLayout
+        overscrollBehavior='none'
+        orientation='vertical'
+        pos='relative'
+        ref={ref}
+        {...otherProps}
+        onPointerDown={(e) => {
+            if(!isRefreshing) {
+                e.currentTarget.setAttribute('data-y-down', e.clientY.toString())
+                setTransition(undefined)
+            }
+        }}
+        style={{
+            touchAction: "none"
+        }}
+        onPointerUp={refresh}
+        onPointerLeave={refresh}
+        onPointerCancel={refresh}
+        onPointerMove={(e) => {
+            const target = e.currentTarget
+            const startYAttribute = target.getAttribute('data-y-down')
+            const startY = startYAttribute ? parseInt(startYAttribute): null
+            if(startY!=null&&target.scrollTop===0) {
+                const currentY = e.clientY - startY
+                const percentage = currentY/180
+                setRefreshPercentage(percentage>1? 1: percentage)
+                console.log(currentY)
+            }
+        }}
+    >
+        <Center
+            pos='absolute'
+            layoutSize={40}
+            shapeScale='full'
+            bg={ThemeTokens.surfaceContainer}
+            top={-60+currentPercentage*180}
+            posH={0}
+            transition={transition}
+            mh='auto'
+            zIndex={1}
+        >
+            <CircularProgressIndicator
+                variant={isRefreshing ? 'indeterminate': 'determinate'}
+                size={24}
+                motionDuration={isRefreshing||transition!==undefined? undefined: '0ms'}
+                value={currentPercentage*100}
+            />
+        </Center>
+        {children}
+    </ScrollLayout>
+})
