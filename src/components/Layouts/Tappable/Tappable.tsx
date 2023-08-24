62b504bb40e38a326c556fdd5ff3b569a5c5c05e
levkopo <levkopoag@gmail.com>
2023-08-24 23:48:54 +0500

    feat(Tappable): new component

---
+++ src/components/Layouts/Tappable/Tappable.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {Layout, LayoutProps} from "../../Basic";
+import {StateLayer} from "../StateLayer/StateLayer";
+
+export interface TappableProps extends LayoutProps {
+    /**
+     * Need to enable ripple
+     *
+     * @default true
+     */
+    ripple?: boolean
+}
+
+export const Tappable = React.forwardRef(
+    (props: TappableProps, ref: React.ForwardedRef<HTMLDivElement>) => {
+        const {
+            children,
+            ripple = true,
+            ...otherProps
+        } = props
+
+        return <Layout
+            ref={ref}
+            cursor={ripple ? 'pointer': undefined}
+            {...otherProps}
+            pos='relative'
+        >
+            <StateLayer ripple={ripple}/>
+            {children}
+        </Layout>
+    }
+)
