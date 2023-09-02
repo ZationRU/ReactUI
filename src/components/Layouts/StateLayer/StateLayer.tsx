08dd77c18032595aed59317430d8142552841068
levkopo <levkopoag@gmail.com>
2023-09-03 00:35:13 +0500

    fix(StateLayer): fix ripple lags

--- src/components/Layouts/StateLayer/StateLayer.tsx
+++ src/components/Layouts/StateLayer/StateLayer.tsx
@@ -1,4 +1,4 @@
-import React, {PointerEvent, MutableRefObject, useRef} from 'react';
+import React, {PointerEvent, MutableRefObject, useRef, useMemo} from 'react';
 import "./StateLayer.css";
 import {Layout} from "../../Basic";
 import {ThemeTokens} from "../../../theme";
@@ -34,6 +34,7 @@ export function StateLayer(props: StateLayerProps) {
                      onPointerUp={state.performUp}
                      onPointerCancel={state.performUp}
                      onPointerLeave={state.performUp}
+                     onMouseOver={state.performUp}
                      onPointerDown={ripple ? state.performDown : undefined}
                 />
             </>
@@ -44,71 +45,74 @@ export function StateLayer(props: StateLayerProps) {
 export interface StateLayerStateData {
     rippleTriggerRef: MutableRefObject<HTMLDivElement | null>
     performDown: (event: PointerEvent<HTMLDivElement>) => void
-    performUp: (event: PointerEvent<HTMLDivElement>) => void
+    performUp: () => void
 }
 
-export const useStateLayer = (): StateLayerStateData => {
+export const useStateLayer = () => {
     const rippleTriggerRef = useRef<HTMLDivElement | null>(null)
-    const msOfRipple = 400
-    const ripples: {
-        startTime: number,
-        element: HTMLSpanElement
-    }[] = []
+
+    return useMemo((): StateLayerStateData => {
+        const msOfRipple = 400
+        const ripples: {
+            startTime: number,
+            element: HTMLSpanElement
+        }[] = []
 
 
-    const performDown = (event: PointerEvent<HTMLDivElement>) => {
-        const rippleTarget = event.currentTarget
-        const rect = rippleTarget.getBoundingClientRect();
+        const performDown = (event: PointerEvent<HTMLDivElement>) => {
+            const rippleTarget = event.currentTarget
+            const rect = rippleTarget.getBoundingClientRect();
 
-        const diameter = Math.max(rippleTarget.clientWidth, rippleTarget.clientHeight);
-        const radius = diameter / 2;
-        
+            const diameter = Math.max(rippleTarget.clientWidth, rippleTarget.clientHeight);
+            const radius = diameter / 2;
 
+            if (rippleTriggerRef.current === null) return;
 
-        if (rippleTriggerRef.current === null) return;
+            const rippleSpan = document.createElement('span')
 
-        const rippleSpan = document.createElement('span')
+            rippleSpan.className = "Ripple Ripple-Animation"
+            rippleSpan.style.width = rippleSpan.style.height = `${diameter}px`;
+            rippleSpan.style.left = `${event.clientX - rect.left - radius}px`;
+            rippleSpan.style.top = `${event.clientY - rect.top - radius}px`;
+            rippleSpan.onmouseout = performUp;
+            rippleSpan.onpointercancel = performUp;
+            rippleTriggerRef.current.parentNode!!.insertBefore(rippleSpan, rippleTriggerRef.current)
 
-        rippleSpan.className = "Ripple Ripple-Animation"
-        rippleSpan.style.width = rippleSpan.style.height = `${diameter}px`;
-        rippleSpan.style.left = `${event.clientX - rect.left - radius}px`;
-        rippleSpan.style.top = `${event.clientY - rect.top - radius}px`;
-        rippleTriggerRef.current.parentNode!!.insertBefore(rippleSpan, rippleTriggerRef.current)
+            ripples.push(
+                {
+                    startTime: new Date().getMilliseconds(),
+                    element: rippleSpan
+                }
+            )
+        }
 
-        ripples.push(
-            {
-                startTime: new Date().getMilliseconds(),
-                element: rippleSpan
-            }
-        )
-    }
-
-    const performUp = (event: PointerEvent<HTMLDivElement>) => {
-        const now = new Date().getMilliseconds();
-        ripples.forEach(({ startTime, element}, index) => {
-            const delayTime = -(now - startTime)
-
-            setTimeout(() => {
-                element.className = "Ripple Ripple-Hidden"
+        const performUp = () => {
+            const now = new Date().getMilliseconds();
+            ripples.forEach(({ startTime, element}, index) => {
+                const delayTime = -(now - startTime)
 
                 setTimeout(() => {
-                    element.style.width = element.style.height =
-                        element.style.left = element.style.top = "";
-
-                    element.className = "Ripple"
+                    element.className = "Ripple Ripple-Hidden"
 
                     setTimeout(() => {
-                        element.remove()
-                        ripples.slice(index, 1)
-                    }, 20)
-                }, 300)
-            }, delayTime < 0 ?  msOfRipple - delayTime: 0)
-        })
-    }
+                        element.style.width = element.style.height =
+                            element.style.left = element.style.top = "";
 
-    return {
-        rippleTriggerRef,
-        performDown,
-        performUp
-    }
+                        element.className = "Ripple"
+
+                        setTimeout(() => {
+                            element.remove()
+                            ripples.slice(index, 1)
+                        }, 20)
+                    }, 300)
+                }, delayTime < 0 ?  msOfRipple - delayTime: 0)
+            })
+        }
+
+        return {
+            rippleTriggerRef,
+            performDown,
+            performUp
+        }
+    }, [])
 }
