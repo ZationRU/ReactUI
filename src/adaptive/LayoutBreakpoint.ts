80e3d4f74354f3134936a241ab99d7f524ddb05c
levkopo <levkopoag@gmail.com>
2023-10-23 00:05:01 +0500

    fix: LayoutBreakpoint unknown breakpoint

--- src/adaptive/LayoutBreakpoint.ts
+++ src/adaptive/LayoutBreakpoint.ts
@@ -1,21 +1,21 @@
 export type LayoutBreakpointsBase<T> = {
-    unknown: T,
     esm: T,
     sm: T,
     emd: T,
     md: T,
-    lg: T
+    lg: T,
+    unknown: T,
 }
 
 export type LayoutBreakpoint = keyof LayoutBreakpointsBase<any>
 
 export const LayoutBreakpointsValues: LayoutBreakpointsBase<number> = {
-    unknown: -1,
     esm: 599,
     sm: 904,
     emd: 1239,
     md: 1439,
-    lg: Number.POSITIVE_INFINITY
+    lg: Number.POSITIVE_INFINITY,
+    unknown: NaN,
 }
 
 export const LayoutBreakpointsKeys = Object.keys(LayoutBreakpointsValues)
