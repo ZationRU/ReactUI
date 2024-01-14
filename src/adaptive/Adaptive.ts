7f15c342aebe7909d0391829754dd7576183abc2
levkopo <levkopoag@gmail.com>
2024-01-15 00:24:27 +0500

    feat: Adaptive now can accept false as negative value

--- src/adaptive/Adaptive.ts
+++ src/adaptive/Adaptive.ts
@@ -1,10 +1,11 @@
 import {arrayToObjectNotation, getClosestValue} from "./utils";
 import {LayoutBreakpoint, LayoutBreakpointsKeys} from "./LayoutBreakpoint";
 
+export type NonSetValues = undefined | null | false
 
 export type AdaptiveArray<T> = Array<T | null>;
 export type AdaptiveObject<T> = Partial<Record<LayoutBreakpoint, T>>;
-export type Adaptive<T> = T | AdaptiveArray<T> | AdaptiveObject<T>;
+export type Adaptive<T> = T | AdaptiveArray<T | NonSetValues> | AdaptiveObject<T | NonSetValues> | NonSetValues;
 
 function isAdaptiveObject<T>(adaptive: Adaptive<T>): adaptive is AdaptiveObject<T> {
     const obj = (adaptive as AdaptiveObject<T>)
