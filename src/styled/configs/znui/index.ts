8a4e3dd338ee023a57f475596c7d9440505255c8
levkopo <levkopoag@gmail.com>
2024-01-15 00:12:36 +0500

    feat: maxLines property

--- src/styled/configs/znui/index.ts
+++ src/styled/configs/znui/index.ts
@@ -1,18 +1,22 @@
 import {shapes, ShapesProps} from "./shapes";
 import {insets, InsetsProps} from "./insets";
 import {animation, AnimationProps} from "./animation";
+import {text, TextProps} from "./text";
 
 export * from "./shapes"
+export * from "./text"
 export * from "./insets"
 
 export const znuiPropsConfig = {
     ...shapes,
     ...insets,
     ...animation,
+    ...text
 }
 
 export type ZnUICSSProps =
     ShapesProps &
     InsetsProps &
-    AnimationProps
+    AnimationProps &
+    TextProps
 ;
